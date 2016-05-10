f9ef3f94bc4e36f0bb0ada2215be71e6b65749d5
Wilson Omokoro <wilson.omokoro@andela.com>
2016-05-10 11:14:48 +0100

    Add input file

--- src/main/java/checkpoint/andela/db/DbWriter.java
+++ src/main/java/checkpoint/andela/db/DbWriter.java
@@ -1,6 +1,7 @@
 package checkpoint.andela.db;
 
 //import checkpoint.andela.log.LogWriter;
+import java.sql.*;
 
 /**
  *
@@ -8,6 +9,9 @@ package checkpoint.andela.db;
  */
 public class DbWriter implements Runnable {
     private String threadName;
+    private Connection conn;
+    private Statement stmt;
+    private ResultSet rs;
     
     public DbWriter(String name){
        threadName = name;
@@ -16,7 +20,37 @@ public class DbWriter implements Runnable {
     
     @Override
     public void run() {
-        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/reactiondb";
+            String user = "root";
+            String password = "root";
+            conn = DriverManager.getConnection(url, user, password);
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT VERSION()");
+            if(rs.next()){
+                System.out.println("MySQL version: "+rs.getString(1));
+            }
+        }
+        catch(Exception e){
+            System.out.println("Exception, run method, DbWriter.java: "+ e);
+        }
+        finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            }
+            catch(SQLException sqle){
+               System.out.println("SQLException, run method, DbWriter.java: "+ sqle); 
+            }
+        }
     }
     
     public boolean readFromBuffer() {
@@ -24,6 +58,8 @@ public class DbWriter implements Runnable {
     }
     
     public boolean writeToDb() {
+        
+        
         return true;
     }
 }
